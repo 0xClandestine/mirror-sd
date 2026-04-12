63adc725e23d32eec6918cd2e9e84a1d7154dc81
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2026-04-11 23:56:15 -0400

    chore(ane): delete old kernels

--- ane/src/lib.rs
+++ ane/src/lib.rs
@@ -8,18 +8,5 @@ fn mirror_sd_ane(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<wrapper::ANETensor>()?;
     m.add_class::<wrapper::ANEKernel>()?;
     m.add_function(wrap_pyfunction!(wrapper::compile_dflash_kernels, m)?)?;
-    m.add_function(wrap_pyfunction!(wrapper::test_conv1x1, m)?)?;
-    m.add_function(wrap_pyfunction!(wrapper::test_rmsnorm, m)?)?;
-    m.add_function(wrap_pyfunction!(wrapper::test_rmsnorm_matmul, m)?)?;
-    m.add_function(wrap_pyfunction!(wrapper::test_matmul, m)?)?;
-    m.add_function(wrap_pyfunction!(wrapper::test_swiglu, m)?)?;
-    m.add_function(wrap_pyfunction!(wrapper::test_sdpa, m)?)?;
-    m.add_function(wrap_pyfunction!(wrapper::test_dflash_nlayers, m)?)?;
-    m.add_function(wrap_pyfunction!(wrapper::test_qkv_progressive, m)?)?;
-    m.add_function(wrap_pyfunction!(wrapper::compile_conv1x1_transpose, m)?)?;
-    m.add_function(wrap_pyfunction!(wrapper::compile_conv1x1_concat, m)?)?;
-    m.add_function(wrap_pyfunction!(wrapper::compile_conv1x1_transpose_b, m)?)?;
-    m.add_function(wrap_pyfunction!(wrapper::test_input_pack_k, m)?)?;
-    m.add_function(wrap_pyfunction!(wrapper::compile_incremental_test_kernels, m)?)?;
     Ok(())
 }
